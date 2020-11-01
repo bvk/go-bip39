60fed340bb460d3d911adf3ad2f7db37fb27a0e8
BVK Chaitanya <bvk@steam.zion.sh>
2020-11-01 15:44:49 -0800

    Renamed user name in the module tyler-smith to bvk for godoc website.

--- bip39_test.go
+++ bip39_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 
 	"github.com/tyler-smith/assert"
-	"github.com/tyler-smith/go-bip39/wordlists"
+	"github.com/bvk/go-bip39/wordlists"
 )
 
 type vector struct {
--- example_test.go
+++ example_test.go
@@ -4,7 +4,7 @@ import (
 	"encoding/hex"
 	"fmt"
 
-	"github.com/tyler-smith/go-bip39"
+	"github.com/bvk/go-bip39"
 )
 
 func ExampleNewMnemonic() {
